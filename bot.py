bfe7fba3abf2c6fa7c05a9452f331a24d67c6613
KM513 <mamlook123456@gmail.com>
2023-09-08 19:26:36 -0300

    Final Push

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import discord
-import responses  
+import responses
 from discord import Intents
 intents = Intents.default()
 intents.message_content = True
-
-
+intents.members = True
 
 import os
 from dotenv import load_dotenv
@@ -15,18 +14,17 @@ async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message) 
         await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:  
+    except Exception as e:
         print(e)
 
-def run_discord_bot():  
+def run_discord_bot():
     TOKEN = os.getenv('TOKEN')
-    print(TOKEN)
     client = discord.Client(intents=intents)
-    
+
     @client.event
     async def on_ready():
         print("Bot is ready!")
-        
+
     @client.event
     async def on_message(message):
         print(message.content)
@@ -38,7 +36,7 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f"{username} said: `{user_message}` ({channel})")
+        print(f"{username} said: {user_message} ({channel})")
 
         if user_message.startswith('?'):
             user_message = user_message[1:]
@@ -46,8 +44,13 @@ def run_discord_bot():
         else:
             await send_message(message, user_message, is_private=False)
 
+
+    @client.event
+    async def on_member_join(member):
+        channel = member.guild.system_channel  # get the system channel
+        if channel is not None:
+            await channel.send(f"Welcome to the server, {member.mention}!")
+
+
+
     client.run(TOKEN)
-
-
-
-
